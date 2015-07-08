d6f06754e57a118daec42aee228c95007a21752b
Michael Smith <michael.smith@puppetlabs.com>
2015-07-08 13:32:55 -0700

    (maint) Use TravisCI container-based builds

--- .travis_target.sh
+++ .travis_target.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ev
 
+# Set compiler to GCC 4.8 here, as Travis overrides the global variables.
+export CC=gcc-4.8 CXX=g++-4.8
+
 if [ ${TRAVIS_TARGET} == RELEASE ]; then
   cmake -Wno-dev .
   make cpplint
@@ -9,9 +12,9 @@ else
   cmake -Wno-dev -DCMAKE_BUILD_TYPE=Debug -DCOVERALLS=ON .
 fi
 
-make
+make -j2
 make test ARGS=-V
 
 # Enable coveralls for public repos
-#if [ ${TRAVIS_TARGET} == DEBUG ]; then coveralls --gcov-options '\-lp' -r .. >/dev/null; fi
+#if [ ${TRAVIS_TARGET} == DEBUG ]; then coveralls --gcov gcov-4.8 --gcov-options '\-lp' -r .. >/dev/null; fi
 
