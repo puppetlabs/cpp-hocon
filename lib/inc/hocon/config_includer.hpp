2d843cfdf65681b059dcbb197598ebea3f61ae9c
Michael Smith <michael.smith@puppetlabs.com>
2015-08-17 15:50:03 -0700

    (HC-22) Port ConfigParseOptions
    For now we leave config_includer unimplemented, as it's not being
    exercised.
    
    config_parse_options was kept an immutable object because we already
    wanted to use a shared_ptr for config_includer, to avoid having to make
    it a concrete class, and it keeps the implementation more in-line with
    the original.
    
    Copies should be light-weight, but we may reconsider and make this a
    mutable class to avoid lots of copies if it's never really shared.

---
+++ lib/inc/hocon/config_includer.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+namespace hocon {
+    /**
+     * Implement this interface and provide an instance to
+     * {@link config_parse_options#set_includer config_parse_options.set_includer()} to
+     * customize handling of {@code include} statements in config files. You may
+     * also want to implement {@link config_includer_file} and {@link config_includer_URL}, or not.
+     */
+    class config_includer {
+    public:
+        /**
+         * Returns a new includer that falls back to the given includer. This is how
+         * you can obtain the default includer; it will be provided as a fallback.
+         * It's up to your includer to chain to it if you want to. You might want to
+         * merge any files found by the fallback includer with any objects you load
+         * yourself.
+         *
+         * It's important to handle the case where you already have the fallback
+         * with a "return this", i.e. this method should not create a new object if
+         * the fallback is the same one you already have. The same fallback may be
+         * added repeatedly.
+         *
+         * @param fallback the previous includer for chaining
+         * @return a new includer
+         */
+        virtual std::shared_ptr<config_includer> with_fallback(std::shared_ptr<config_includer> fallback) const = 0;
+
+        /**
+         * Parses another item to be included. The returned object typically would
+         * not have substitutions resolved. You can throw a config_exception here to
+         * abort parsing, or return an empty object, but may not return null.
+         * 
+         * This method is used for a "heuristic" include statement that does not
+         * specify file, or URL resource. If the include statement does
+         * specify, then the same class implementing {@link config_includer} must
+         * also implement {@link config_includer_file} or {@link config_includer_URL} as needed, or a
+         * default includer will be used.
+         * 
+         * @param context
+         *            some info about the include context
+         * @param what
+         *            the include statement's argument
+         * @return a non-null config_object
+         */
+        // TODO: Implement config_include_context, config_object
+        // virtual config_object include(shared_ptr<config_include_context> context, shared_ptr<std::string> what) const = 0;
+    };
+}  // namespace hocon
