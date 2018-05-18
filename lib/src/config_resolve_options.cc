26b0fa1b86290fe64195fa188ee7ba44a34da6c6
Andrey Troitskiy <troizky@mail.ru>
2018-05-18 13:22:02 +0300

    Fix config_resolve_options.cc
    Comparing to https://github.com/lightbend/config/blob/master/config/src/main/java/com/typesafe/config/impl/ConfigReference.java#L93, it looks like the fallback code at https://github.com/puppetlabs/cpp-hocon/blob/master/lib/src/values/config_reference.cc#L61 wasn't implemented.

--- lib/src/config_resolve_options.cc
+++ lib/src/config_resolve_options.cc
@@ -2,11 +2,13 @@
 
 namespace hocon {
 
-    config_resolve_options::config_resolve_options(bool use_system_environment, bool allow_unresolved) :
-        _use_system_environment(use_system_environment), _allow_unresovled(allow_unresolved) { }
+    const shared_resolver config_resolve_options::NULL_RESOLVER = make_shared<config_resolver>();
+
+    config_resolve_options::config_resolve_options(bool use_system_environment, bool allow_unresolved, shared_resolver resolver) :
+        _use_system_environment(use_system_environment), _allow_unresovled(allow_unresolved), _resolver(resolver) { }
 
     config_resolve_options config_resolve_options::set_use_system_environment(bool value) const {
-        return config_resolve_options(value, _allow_unresovled);
+        return config_resolve_options(value, _allow_unresovled, _resolver);
     }
 
     bool config_resolve_options::get_use_system_environment() const {
@@ -14,11 +16,25 @@ namespace hocon {
     }
 
     config_resolve_options config_resolve_options::set_allow_unresolved(bool value) const {
-        return config_resolve_options(_use_system_environment, value);
+        return config_resolve_options(_use_system_environment, value, _resolver);
     }
 
     bool config_resolve_options::get_allow_unresolved() const {
         return _allow_unresovled;
+    }
+    shared_resolver config_resolve_options::get_resolver() const {
+        return _resolver;
+    }
+
+    config_resolve_options config_resolve_options::append_resolver(shared_resolver value) {
+        if (value == nullptr) {
+            throw bug_or_broken_exception(_("null resolver passed to append_resolver"));
+        } else if (value == _resolver) {
+            return *this;
+        } else {
+            return config_resolve_options(_use_system_environment, _allow_unresovled,
+                                            _resolver->with_fallback(value));
+        }
     }
 
 }  // namespace hocon
